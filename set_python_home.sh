2362c2622728ef80dc3dfadb6a87c4f157598cc9
Ivo Bellin Salarin <ivo@lumapps.com>
2018-02-13 17:15:10 +0100

    troubleshooting and better python path detection

--- set_python_home.sh
+++ set_python_home.sh
@@ -1,4 +1,8 @@
-# macOS python
-export PYTHON_HOME=/System/Library/Frameworks/Python.framework/Versions/2.7
-# homebrew python
-export PYTHON_HOME=/usr/local/Cellar/python/2.7.13_1/Frameworks/Python.framework/Versions/Current
+PYTHON_EXECUTABLE=`which python`
+if $PYTHON_EXECUTABLE == "/usr/bin/python"; then
+    # macOS System python
+    export PYTHON_HOME=/System/Library/Frameworks/Python.framework/Versions/2.7
+else
+    # homebrew python
+    export PYTHON_HOME=/usr/local
+fi

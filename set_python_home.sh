792dbf5f7944b0c29b05200ef7fa264b7a0c8e05
Ivo Bellin Salarin <ivo@lumapps.com>
2018-02-27 09:05:09 +0100

    chore: bash scripts

--- set_python_home.sh
+++ set_python_home.sh
@@ -1,5 +1,5 @@
 PYTHON_EXECUTABLE=`which python`
-if $PYTHON_EXECUTABLE == "/usr/bin/python"; then
+if [ "$PYTHON_EXECUTABLE" == "/usr/bin/python" ]; then
     # macOS System python
     export PYTHON_HOME=/System/Library/Frameworks/Python.framework/Versions/2.7
 else

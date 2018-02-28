78bc345b45db695145ccd096bd07020275095fc7
Marc-Olivier Castagnetti <6144489+mcastagnetti@users.noreply.github.com>
2018-02-28 09:27:46 +0100

    chore: reinstalling correct fsnotify/fsevents dependency on build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 . ./set_python_home.sh
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PYTHON_HOME/lib/pkgconfig
 set -eu
+go get github.com/fsnotify/fsevents
 go build -buildmode=c-shared -o fsevents_watcher.so

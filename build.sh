d669e5f4688f39f93af4ac1678622c5610381795
Ivo Bellin Salarin <ivo@lumapps.com>
2019-02-21 10:42:46 +0100

    fix: capture dependencies

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 . ./set_python_home.sh
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PYTHON_HOME/lib/pkgconfig
 set -eu
-go get github.com/fsnotify/fsevents
+dep ensure
 go build -buildmode=c-shared -o fsevents_watcher.so  # in case of problems, add here -x

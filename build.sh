752df1b7320ba2584c07d41efc2f4c3b9ba83643
Ivo Bellin Salarin <ivo@lumapps.com>
2019-10-15 17:05:36 +0200

    chore: upgrade to go mod

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 . ./set_python_home.sh
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PYTHON_HOME/lib/pkgconfig
 set -eu
-dep ensure
 go build -buildmode=c-shared -o fsevents_watcher.so $@ # in case of problems, add here -x
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nilleb/fsevents-watcher/v2
+
+go 1.13
+
+require github.com/fsnotify/fsevents v0.1.1
